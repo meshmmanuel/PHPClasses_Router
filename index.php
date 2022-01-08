d639b8353594fac1b7699c0c7a22b7532652c30b
Mesh Manuel <meshmmanuel@yahoo.com>
2022-01-08 23:55:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,3 +5,12 @@ import 'Router.php';
 Router::get('/', function(){
  // perform action for home path
 });
+
+
+Router::post('/post', function(){
+ // perform action for home path
+});
+
+
+Router::get('/', 'Controller@action');
+
